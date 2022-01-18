749331ea29e96727c562a874d88636d244e1d49e
Maciej Pyrc <m.pyrc@interia.pl>
2022-01-18 21:01:27 +0100

    style: 💄 Simple styles for logger

--- packages/core/lib/managers/logger/logger.manager.constants.ts
+++ packages/core/lib/managers/logger/logger.manager.constants.ts
@@ -1,9 +1,11 @@
 import { LoggerLevelType } from "./logger.manager.types";
 
-export const loggerColors: Record<LoggerLevelType, string> = {
-  error: "red",
-  warning: "orange",
-  http: "black",
-  info: "blue",
-  debug: "gray",
+const defaultStyles = "background:rgba(0,0,0,0.2);padding:2px 5px;border-radius:5px;font-weight:bold;";
+
+export const loggerStyles: Record<LoggerLevelType, string> = {
+  error: `${defaultStyles}color:#db2525`,
+  warning: `${defaultStyles}color:#e1941e`,
+  http: `${defaultStyles}color:#c743cf`,
+  info: `${defaultStyles}color:#1e74e1`,
+  debug: `${defaultStyles}color:#adadad`,
 };
--- packages/core/lib/managers/logger/logger.manager.utils.ts
+++ packages/core/lib/managers/logger/logger.manager.utils.ts
@@ -1,13 +1,14 @@
-import { LoggerType } from "managers";
+import { loggerStyles, LoggerType } from "managers";
 
 // Logger
 export const logger: LoggerType = (log) => {
+  const styles = loggerStyles[log.level];
   const timestamp = new Date().toLocaleString();
-  const module = `[${log.module}]:[${timestamp}]:`;
+  const module = `%c[${log.module}]:[${timestamp}]:`;
   // eslint-disable-next-line no-console
-  console.log(`${module} ${log.message}`);
+  console.log(module, styles, log.message);
   log.additionalData?.forEach((data) => {
     // eslint-disable-next-line no-console
-    console.log(module, data);
+    console.log(module, styles, data);
   });
 };
