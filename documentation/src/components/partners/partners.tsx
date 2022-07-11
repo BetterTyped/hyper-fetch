68cd18a2121df9816f99be4893f002e8641d4d85
Maciej Pyrc <maciekpyrc@gmail.com>
2022-07-11 20:03:01 +0200

    fix: 🐛 Broken links

--- documentation/src/components/partners/partners.tsx
+++ documentation/src/components/partners/partners.tsx
@@ -7,7 +7,7 @@ export function Partners(): JSX.Element {
   return (
     <section className={clsx(styles.container)}>
       <div className="container">
-        <h2 className="page-section-title">Be a partner in our solutions.</h2>
+        <h2 className="page-section-title">Be our partner.</h2>
         <div className="page-section-subtitle">
           BetterTyped was created to make the best open source tools. However, it is an extremely demanding task, so we
           encourage you to partner and sponsor our work.
