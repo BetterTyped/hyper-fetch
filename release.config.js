7404a16ea56e82a00c512301a6c4cda2c246f805
Maciej Pyrc <maciekpyrc@gmail.com>
2022-11-06 21:26:09 +0100

    ci: 🎡 Semantic release config

--- release.config.js
+++ release.config.js
@@ -48,7 +48,12 @@ module.exports = {
         },
       ],
       "@semantic-release/release-notes-generator",
-      "@semantic-release/npm",
+      [
+        "@semantic-release/npm",
+        {
+          npmPublish: true,
+        },
+      ],
     ],
   },
 };
