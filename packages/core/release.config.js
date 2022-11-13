02c76c9df5996014b47d290f7cb5d4da408eabc8
Maciej Pyrc <maciekpyrc@gmail.com>
2022-11-13 16:25:16 +0100

    ci: 🎡 Release adjustments

--- packages/core/release.config.js
+++ packages/core/release.config.js
@@ -2,13 +2,5 @@
 const config = require("../../release.config");
 
 const newConfig = { ...config, publish: undefined, success: undefined, fail: undefined };
-// newConfig.release.plugins.pop();
-// // We want to publish only single release notes
-// newConfig.release.plugins.push([
-//   "@semantic-release/github",
-//   {
-//     publish: true,
-//   },
-// ]);
 
 module.exports = newConfig;
