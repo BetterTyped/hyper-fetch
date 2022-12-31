7039f2f8cd3604b4d06e50eb9cadb416643ed31a
Maciej Pyrc <maciekpyrc@gmail.com>
2022-12-31 12:37:32 +0100

    docs: ✏️ ProductHunt badge

--- documentation/src/components/header/header.tsx
+++ documentation/src/components/header/header.tsx
@@ -18,6 +18,19 @@ export function Header(): JSX.Element {
 
   return (
     <header className={clsx(styles.container)}>
+      <a
+        href="https://www.producthunt.com/posts/hyper-fetch?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-hyper&#0045;fetch"
+        target="_blank"
+        rel="noreferrer"
+        className={styles.productHunt}
+      >
+        <img
+          src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=373208&theme=dark"
+          alt="Hyper&#0032;Fetch - Framework&#0032;for&#0032;data&#0032;exchange&#0044;&#0032;async&#0032;state&#0032;management&#0032;and&#0032;more | Product Hunt"
+          width="250"
+          height="54"
+        />
+      </a>
       <div className={clsx("container", styles.wrapper)}>
         <h2 className={clsx(styles.title)}>
           <div className={clsx(styles.projectName)}>{siteConfig.projectName}</div>
