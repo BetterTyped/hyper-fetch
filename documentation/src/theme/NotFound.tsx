244f2d3d40f125c04547c7b547dff59518f34067
kacper <kacper.skawina@gmail.com>
2023-01-03 22:13:59 +0100

    docs: ✏️ Added correct titles for sidebar & notFound redirect

---
+++ documentation/src/theme/NotFound.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import NotFound from '@theme-original/NotFound';
+
+export default function NotFoundWrapper(props) {
+  return (
+    <>
+      <NotFound {...props} />
+    </>
+  );
+}
