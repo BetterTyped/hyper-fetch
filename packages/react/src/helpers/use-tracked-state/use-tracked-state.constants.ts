3189e6b727f43e3e08829af2fd477a6a19401db3
Maciej Pyrc <maciekpyrc@gmail.com>
2023-03-07 12:13:49 +0100

    AdditionalData added to the response

--- packages/react/src/helpers/use-tracked-state/use-tracked-state.constants.ts
+++ packages/react/src/helpers/use-tracked-state/use-tracked-state.constants.ts
@@ -1,10 +1,10 @@
 import { UseTrackedStateType } from "./use-tracked-state.types";
 
-export const initialState: <T>() => UseTrackedStateType<T> = () => ({
+export const initialState: UseTrackedStateType = {
   data: null,
   error: null,
   loading: false,
   additionalData: {},
   retries: 0,
   timestamp: null,
-});
+};
