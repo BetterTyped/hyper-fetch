09513f8e79a588f81bed6f447be5af5dc7e04a81
Maciej Pyrc <maciekpyrc@gmail.com>
2023-05-04 21:52:47 +0200

    refactor: 💡 Typescript adjustments

--- packages/firebase/src/adapter/adapter.firebase.admin.ts
+++ packages/firebase/src/adapter/adapter.firebase.admin.ts
@@ -1,61 +1,57 @@
-import {
-  ExtractAdapterType,
-  ExtractErrorType,
-  ExtractResponseType,
-  getAdapterBindings,
-  RequestInstance,
-  ResponseReturnType,
-} from "@hyper-fetch/core";
+import { getAdapterBindings, ResponseReturnType } from "@hyper-fetch/core";
 import { Firestore } from "firebase-admin/firestore";
 
-import { FirestoreDBMethods, FirestoreQueryParams } from "./types/adapter.firestore.types";
+import { FirestoreAdapterType, FirestoreDBMethods, FirestoreQueryParams } from "./types/adapter.firestore.types";
 import { FirebaseAdminAdapterTypes, FirebaseAdminDBTypes } from "./types/adapter.base.types.admin";
 import { getFirestoreMethodsAdmin } from "./methods/adapter.methods.firestore.admin";
-import { RealtimeDBMethods, RealtimeDBQueryParams } from "./types/adapter.realtimedb.types";
+import { RealtimeDbAdapterType, RealtimeDBMethods, RealtimeDBQueryParams } from "./types/adapter.realtimedb.types";
 import { getRealtimeDBMethodsAdmin } from "./methods/adapter.methods.realtime.admin";
 
 // TODO - add pre and post validation for firebase
 
 export const firebaseAdminAdapter = <T extends FirebaseAdminDBTypes>(database: T) => {
-  const adapter: FirebaseAdminAdapterTypes<T> = async <R extends RequestInstance>(request: R, requestId: string) => {
-    const { fullUrl, onSuccess, onError } = await getAdapterBindings(request, requestId, 0, {});
-    return new Promise<ResponseReturnType<ExtractResponseType<R>, ExtractErrorType<R>, ExtractAdapterType<R>>>(
-      (resolve) => {
-        // eslint-disable-next-line no-console
-        if (database instanceof Firestore) {
-          const {
-            method = "onSnapshot",
-            queryParams,
-            data,
-          }: { method: FirestoreDBMethods; queryParams: FirestoreQueryParams; data } = request;
-          const availableMethods = getFirestoreMethodsAdmin(request, database, fullUrl, onSuccess, onError, resolve);
-          const selectedMethod = availableMethods[method];
-          if (!selectedMethod) {
-            throw new Error(`Cannot find method ${method} in Firestore available methods.`);
-          }
-          selectedMethod({
-            constraints: queryParams?.constraints ? queryParams.constraints : [],
-            data,
-          });
-        } else {
-          // TODO - fix selecting realtime
-          const {
-            method = "onValue" as RealtimeDBMethods,
-            queryParams,
-            data,
-          }: { method: RealtimeDBMethods; queryParams: RealtimeDBQueryParams; data } = request;
-          const availableMethods = getRealtimeDBMethodsAdmin(request, database, fullUrl, onSuccess, onError, resolve);
-          const selectedMethod = availableMethods[method];
-          if (!selectedMethod) {
-            throw new Error(`Cannot find method ${method} in Realtime database available methods.`);
-          }
-          selectedMethod({
-            constraints: queryParams?.constraints ? queryParams.constraints : [],
-            data,
-          });
-        }
-      },
+  const adapter: FirebaseAdminAdapterTypes<T> = async (request, requestId) => {
+    const { fullUrl, onSuccess, onError } = await getAdapterBindings<RealtimeDbAdapterType | FirestoreAdapterType>(
+      request,
+      requestId,
+      "error",
+      {},
     );
+    return new Promise<ResponseReturnType<any, any, FirebaseAdminAdapterTypes<T>>>((resolve) => {
+      // eslint-disable-next-line no-console
+      if (database instanceof Firestore) {
+        const {
+          method = "onSnapshot",
+          queryParams,
+          data,
+        }: { method: FirestoreDBMethods; queryParams: FirestoreQueryParams; data } = request;
+        const availableMethods = getFirestoreMethodsAdmin(request, database, fullUrl, onSuccess, onError, resolve);
+        const selectedMethod = availableMethods[method];
+        if (!selectedMethod) {
+          throw new Error(`Cannot find method ${method} in Firestore available methods.`);
+        }
+        selectedMethod({
+          constraints: queryParams?.constraints ? queryParams.constraints : [],
+          data,
+        });
+      } else {
+        // TODO - fix selecting realtime
+        const {
+          method = "onValue" as RealtimeDBMethods,
+          queryParams,
+          data,
+        }: { method: RealtimeDBMethods; queryParams: RealtimeDBQueryParams; data } = request;
+        const availableMethods = getRealtimeDBMethodsAdmin(request, database, fullUrl, onSuccess, onError, resolve);
+        const selectedMethod = availableMethods[method];
+        if (!selectedMethod) {
+          throw new Error(`Cannot find method ${method} in Realtime database available methods.`);
+        }
+        selectedMethod({
+          constraints: queryParams?.constraints ? queryParams.constraints : [],
+          data,
+        });
+      }
+    });
   };
   return adapter;
 };
--- packages/firebase/src/adapter/adapter.firebase.web.ts
+++ packages/firebase/src/adapter/adapter.firebase.web.ts
@@ -1,62 +1,58 @@
-import {
-  ExtractAdapterType,
-  ExtractErrorType,
-  ExtractResponseType,
-  getAdapterBindings,
-  RequestInstance,
-  ResponseReturnType,
-} from "@hyper-fetch/core";
+import { getAdapterBindings, ResponseReturnType } from "@hyper-fetch/core";
 import { Database } from "firebase/database";
 import { Firestore } from "firebase/firestore";
 
 import { getRealtimeDBMethodsWeb } from "./methods/adapter.methods.realtime.web";
 import { FirebaseAdapterTypes, FirebaseWebDBTypes } from "./types/adapter.base.types.web";
-import { RealtimeDBMethods, RealtimeDBQueryParams } from "./types/adapter.realtimedb.types";
-import { FirestoreDBMethods, FirestoreQueryParams } from "./types/adapter.firestore.types";
+import { RealtimeDbAdapterType, RealtimeDBMethods, RealtimeDBQueryParams } from "./types/adapter.realtimedb.types";
+import { FirestoreAdapterType, FirestoreDBMethods, FirestoreQueryParams } from "./types/adapter.firestore.types";
 import { getFirestoreMethodsWeb } from "./methods/adapter.methods.firestore.web";
 
 // TODO - add pre and post validation for firebase
 
 export const firebaseWebAdapter = <T extends FirebaseWebDBTypes>(database: T) => {
-  const adapter: FirebaseAdapterTypes<T> = async <R extends RequestInstance>(request: R, requestId: string) => {
-    const { fullUrl, onSuccess, onError } = await getAdapterBindings(request, requestId, 0, {});
-    return new Promise<ResponseReturnType<ExtractResponseType<R>, ExtractErrorType<R>, ExtractAdapterType<R>>>(
-      (resolve) => {
-        // eslint-disable-next-line no-console
-        if (database instanceof Database) {
-          const {
-            method = "onValue" as RealtimeDBMethods,
-            queryParams,
-            data,
-          }: { method: RealtimeDBMethods; queryParams: RealtimeDBQueryParams; data } = request;
-          const availableMethods = getRealtimeDBMethodsWeb(request, database, fullUrl, onSuccess, onError, resolve);
-          const selectedMethod = availableMethods[method];
-          if (!selectedMethod) {
-            throw new Error(`Cannot find method ${method} in Realtime DB available methods.`);
-          }
-          selectedMethod({
-            constraints: queryParams?.constraints || [],
-            data,
-          });
-        }
-        if (database instanceof Firestore) {
-          const {
-            method = "onSnapshot",
-            queryParams,
-            data,
-          }: { method: FirestoreDBMethods; queryParams: FirestoreQueryParams; data } = request;
-          const availableMethods = getFirestoreMethodsWeb(request, database, fullUrl, onSuccess, onError, resolve);
-          const selectedMethod = availableMethods[method];
-          if (!selectedMethod) {
-            throw new Error(`Cannot find method ${method} in Firestore available methods.`);
-          }
-          selectedMethod({
-            constraints: queryParams?.constraints ? queryParams.constraints : [],
-            data,
-          });
-        }
-      },
+  const adapter: FirebaseAdapterTypes<T> = async (request, requestId) => {
+    const { fullUrl, onSuccess, onError } = await getAdapterBindings<RealtimeDbAdapterType | FirestoreAdapterType>(
+      request,
+      requestId,
+      "error",
+      {},
     );
+    return new Promise<ResponseReturnType<any, any, FirebaseAdapterTypes<T>>>((resolve) => {
+      // eslint-disable-next-line no-console
+      if (database instanceof Database) {
+        const {
+          method = "onValue" as RealtimeDBMethods,
+          queryParams,
+          data,
+        }: { method: RealtimeDBMethods; queryParams: RealtimeDBQueryParams; data } = request;
+        const availableMethods = getRealtimeDBMethodsWeb(request, database, fullUrl, onSuccess, onError, resolve);
+        const selectedMethod = availableMethods[method];
+        if (!selectedMethod) {
+          throw new Error(`Cannot find method ${method} in Realtime DB available methods.`);
+        }
+        selectedMethod({
+          constraints: queryParams?.constraints || [],
+          data,
+        });
+      }
+      if (database instanceof Firestore) {
+        const {
+          method = "onSnapshot",
+          queryParams,
+          data,
+        }: { method: FirestoreDBMethods; queryParams: FirestoreQueryParams; data } = request;
+        const availableMethods = getFirestoreMethodsWeb(request, database, fullUrl, onSuccess, onError, resolve);
+        const selectedMethod = availableMethods[method];
+        if (!selectedMethod) {
+          throw new Error(`Cannot find method ${method} in Firestore available methods.`);
+        }
+        selectedMethod({
+          constraints: queryParams?.constraints ? queryParams.constraints : [],
+          data,
+        });
+      }
+    });
   };
   return adapter;
 };
