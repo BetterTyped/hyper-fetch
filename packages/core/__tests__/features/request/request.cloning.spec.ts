0c864e3ae1c2b91c823387223939c603ed87ff8e
Maciej Pyrc <maciekpyrc@gmail.com>
2023-06-24 11:33:41 +0200

    test: 💍 Adjusted test cases

--- packages/core/__tests__/features/request/request.cloning.spec.ts
+++ packages/core/__tests__/features/request/request.cloning.spec.ts
@@ -23,9 +23,9 @@ describe("Request [ Cloning ]", () => {
 
   describe("When cloning request", () => {
     it("should generate new keys", async () => {
-      const clone = request.setParams({ userId: 1 } as null).clone();
+      const clone = request.setParams({ userId: 1 }).clone();
       expect(clone.abortKey).toBe(request.abortKey);
-      expect(clone.queueKey).toBe(request.queueKey);
+      expect(clone.queueKey).not.toBe(request.queueKey);
       expect(clone.cacheKey).not.toBe(request.cacheKey);
     });
     it("should not generate new keys when we set custom ones", async () => {
