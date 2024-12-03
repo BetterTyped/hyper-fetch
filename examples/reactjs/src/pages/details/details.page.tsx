61f5f341874db373b487f530c4a7ea65773a875e
Maciej Pyrc <maciekpyrc@gmail.com>
2024-12-03 11:29:49 +0100

    refactor: 💡 bring back old example code

--- examples/reactjs/src/pages/details/details.page.tsx
+++ examples/reactjs/src/pages/details/details.page.tsx
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
+import Countdown from "react-countdown";
 import { DateInterval } from "@hyper-fetch/core";
 import { useFetch } from "@hyper-fetch/react";
+import { Stack, Button, Typography } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import RefreshIcon from "@mui/icons-material/Refresh";
 
+import { Viewer } from "components/viewer";
+import { Request } from "components/request";
 import { getUser } from "api";
 
 const refreshTime = DateInterval.second * 10;
@@ -10,28 +16,34 @@ const initialDate = +new Date();
 export const DetailsPage: React.FC = () => {
   const [dep, setDep] = useState(initialDate);
 
-  const result = useFetch(getUser, {
-    // dependencies: [dep],
-    // refresh: true,
+  const result = useFetch(getUser.setQueryParams({ date: dep } as any), {
+    dependencies: [dep],
+    refresh: true,
     refreshTime,
+    revalidateOnMount: false,
   });
 
-  console.log({ data: result.data, loading: result.loading });
+  const { revalidate, timestamp } = result;
 
-  if (result.loading) {
-    console.log("Loading new data");
-    return "Loading";
-  }
-
-  if (result.error) {
-    console.log("An error was found");
-    return "Error";
-  }
-
-  if (!result.data) {
-    console.log("No data found with the request!");
-    return "No data found";
-  }
-
-  return JSON.stringify(result.data, null, 2);
+  return (
+    <Viewer name="Details">
+      <Request name="Get one" result={result}>
+        <Stack direction="row" spacing={1} sx={{ mb: 3 }}>
+          <Button variant="outlined" onClick={() => setDep(+new Date())}>
+            Change dependency
+          </Button>
+          <IconButton onClick={() => revalidate()}>
+            <RefreshIcon />
+          </IconButton>
+        </Stack>
+      </Request>
+      <Typography>
+        <b>Refresh in:</b>{" "}
+        <Countdown
+          date={timestamp ? +timestamp + refreshTime : Date.now() + refreshTime}
+          key={String(timestamp?.getTime())}
+        />
+      </Typography>
+    </Viewer>
+  );
 };
