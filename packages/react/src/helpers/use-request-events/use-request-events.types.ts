25e3504c82653ca53f7f7f3015f91e674051032e
Maciej Pyrc <maciekpyrc@gmail.com>
2024-12-06 15:41:25 +0100

    Merge branch 'main' into 6.0.0

--- packages/react/src/helpers/use-request-events/use-request-events.types.ts
+++ packages/react/src/helpers/use-request-events/use-request-events.types.ts
@@ -77,9 +77,9 @@ export type UseRequestEventsActionsType<T extends RequestInstance> = {
 export type UseRequestEventsReturnType<T extends RequestInstance> = [
   UseRequestEventsActionsType<T>,
   {
-    addCacheDataListener: (request: RequestInstance) => VoidFunction;
+    addCacheDataListener: (request: T) => VoidFunction;
     clearCacheDataListener: VoidFunction;
-    addLifecycleListeners: (request: RequestInstance, requestId?: string) => VoidFunction;
+    addLifecycleListeners: (request: T, requestId?: string) => VoidFunction;
     removeLifecycleListener: (requestId: string) => void;
     clearLifecycleListeners: () => void;
   },
