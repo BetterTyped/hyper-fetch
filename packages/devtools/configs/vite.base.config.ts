e76265f20d9951acdeb01ba975251377f3c388a1
Maciej Pyrc <maciekpyrc@gmail.com>
2025-04-25 18:41:58 +0200

    progress

--- packages/devtools/configs/vite.base.config.ts
+++ packages/devtools/configs/vite.base.config.ts
@@ -3,17 +3,12 @@ import type { UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export const config: UserConfig = {
-  build: {
-    rollupOptions: {
-      input: "src/app/index.html",
-    },
-  },
   plugins: [
     tsconfigPaths({
       root: path.resolve(__dirname, "../"),
     }),
   ],
   resolve: {
-    preserveSymlinks: true, // this is the fix!
+    preserveSymlinks: true,
   },
 };
