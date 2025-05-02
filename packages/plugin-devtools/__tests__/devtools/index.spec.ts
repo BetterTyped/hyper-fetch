96acde1dd97ec6a1aee57af68e5bddd0ccb8944e
Maciej Pyrc <maciekpyrc@gmail.com>
2025-05-02 20:39:29 +0200

    progress

--- packages/plugin-devtools/__tests__/devtools/index.spec.ts
+++ packages/plugin-devtools/__tests__/devtools/index.spec.ts
@@ -1,51 +1,42 @@
 import { Client } from "@hyper-fetch/core";
-import { Socket } from "@hyper-fetch/sockets";
-import { createHttpMockingServer } from "@hyper-fetch/testing";
-import { WebSocketServer } from "ws";
-import waitForExpect from "wait-for-expect";
+import { createHttpMockingServer, createWebsocketMockingServer } from "@hyper-fetch/testing";
 
-// import { BackendDevtools } from "../../src/index";
-import { initializeWebsocketServer } from "../websockets.initialize";
+import { devtoolsPlugin } from "../../src/index";
 
-const { resetMocks, startServer, stopServer, mockRequest } = createHttpMockingServer();
+const {
+  startServer: startWebsocketServer,
+  stopServer: stopWebsocketServer,
+  // emitListenerEvent,
+  // expectEmitterEvent,
+} = createWebsocketMockingServer("ws://localhost:2137");
+const {
+  resetMocks,
+  startServer,
+  stopServer,
+  // mockRequest
+} = createHttpMockingServer();
 
 describe("Backend Devtools - Socket Test", () => {
-  let wss: WebSocketServer;
-  const socket = new Socket({ url: "ws://localhost:1234" });
-  const socketListener = socket.createListener()({ topic: "test" });
-  let client = new Client({ url: "shared-base-url" });
+  let client = new Client({ url: "shared-base-url" }).addPlugin(devtoolsPlugin({ appName: "TestApp" }));
   let request = client.createRequest()({ endpoint: "/shared-endpoint" });
-  // let backendDevtools = new BackendDevtools(client);
 
   beforeAll(() => {
-    wss = initializeWebsocketServer(1234);
     startServer();
+    startWebsocketServer();
   });
   beforeEach(() => {
     client = new Client({ url: "shared-base-url" });
-    // backendDevtools = new BackendDevtools(client);
     request = client.createRequest()({ endpoint: "/shared-endpoint" });
     jest.resetAllMocks();
     jest.clearAllMocks();
     resetMocks();
   });
-  afterEach(() => {
-    // backendDevtools.unmountHooksFromClient();
-  });
-
   afterAll(() => {
-    wss.close();
+    stopWebsocketServer();
     stopServer();
   });
-  it("test call for backend dev", async () => {
-    const spy = jest.fn();
-    const removeListener = socketListener.listen(spy);
-    mockRequest(request);
-    await request.send();
-
-    await waitForExpect(() => {
-      expect(spy).toHaveBeenCalled();
-    }, 1000);
-    removeListener();
+  it("should send REQUEST_CREATED event when a request is created", async () => {
+    console.warn("test", request);
+    expect(true).toBe(true);
   });
 });
