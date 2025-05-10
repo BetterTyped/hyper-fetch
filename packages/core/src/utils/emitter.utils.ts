833a11724883d51f4255befaf28cc3fdf97508c4
Maciej Pyrc <maciekpyrc@gmail.com>
2025-05-10 17:24:21 +0200

    progress

--- packages/core/src/utils/emitter.utils.ts
+++ packages/core/src/utils/emitter.utils.ts
@@ -3,7 +3,7 @@ import Emitter from "events";
 const getListenName = (event: string | symbol) => `listen_${String(event)}`;
 
 export class EventEmitter extends Emitter {
-  emitCallbacks?: Array<(event: string, data: any, isTriggeredExternally: boolean) => void>;
+  emitCallbacks: Array<(event: string, data: any, isTriggeredExternally: boolean) => void> = [];
 
   // eslint-disable-next-line @typescript-eslint/no-useless-constructor
   constructor(options?: ConstructorParameters<typeof Emitter>[0]) {
