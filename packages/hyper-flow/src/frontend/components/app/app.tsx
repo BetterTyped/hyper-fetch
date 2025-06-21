799392ced439e9f52f1ecc14ea53c617437abdb1
Maciej Pyrc <maciekpyrc@gmail.com>
2025-06-21 17:24:17 +0200

    progress

--- packages/hyper-flow/src/frontend/components/app/app.tsx
+++ packages/hyper-flow/src/frontend/components/app/app.tsx
@@ -1,5 +1,5 @@
 import { Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+// import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 import { Navbar } from "./navbar/navbar";
 import { useIsFocused } from "@/hooks/use-is-focused";
