07311d5f7d0dd2d1eb039f5519d1157dc0477efe
Maciej Pyrc <maciekpyrc@gmail.com>
2025-06-21 18:58:14 +0200

    progress

--- packages/hyper-flow/jest.config.ts
+++ packages/hyper-flow/jest.config.ts
@@ -3,4 +3,12 @@ import type { Config } from "@jest/types";
 import { getJestConfig } from "../../jest.config";
 
 const config: Config.InitialOptions = getJestConfig();
+
+config.moduleNameMapper = {
+  "^@\/(.*)$": "<rootDir>/src/frontend/$1",
+  "^@shared\/(.*)$": "<rootDir>/src/shared/$1",
+  "^@server\/(.*)$": "<rootDir>/src/server/$1",
+  "^@app\/(.*)$": "<rootDir>/src/app/$1",
+};
+
 export default config;
