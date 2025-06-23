51d478428db44d59312542d5499b3bbead040cd6
Maciej Pyrc <maciekpyrc@gmail.com>
2025-06-23 17:34:23 +0200

    refactor: 💡 adjustments to packages

--- packages/hyper-flow/src/frontend/components/ui/chart.tsx
+++ packages/hyper-flow/src/frontend/components/ui/chart.tsx
@@ -170,7 +170,7 @@ function ChartTooltipContent({
               )}
             >
               {formatter && item?.value !== undefined && item.name ? (
-                formatter(item.value, item.name, item, index, item.payload)
+                <>{formatter(item.value, item.name, item, index, item.payload)}</>
               ) : (
                 <>
                   {itemConfig?.icon ? (
