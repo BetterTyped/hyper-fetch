aa3333dae1bc05dd1302b571159c9727725f29a6
Maciej Pyrc <maciekpyrc@gmail.com>
2025-07-06 21:05:30 +0200

    style: 💄 css

--- packages/hyper-flow/src/frontend/components/ui/title.tsx
+++ packages/hyper-flow/src/frontend/components/ui/title.tsx
@@ -7,7 +7,7 @@ export const Title = ({ children, ...props }: HTMLProps<HTMLHeadingElement>) => 
     <h2
       {...props}
       className={cn(
-        "text-xl md:text-3xl text-left animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-zinc-200),var(--color-zinc-300),var(--color-zinc-50),var(--color-zinc-400),var(--color-zinc-200))] bg-[length:200%_auto] bg-clip-text font-nacelle font-semibold text-transparent",
+        "text-xl md:text-3xl text-left animate-[gradient_6s_linear_infinite] bg-[linear-gradient(to_right,var(--color-zinc-200),var(--color-zinc-300),var(--color-zinc-50),var(--color-zinc-400),var(--color-zinc-200))] bg-[length:200%_auto] bg-clip-text font-semibold text-transparent",
         props.className,
       )}
     >
